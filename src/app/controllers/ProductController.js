5179f2b241e60ae4c7b289a23a82ffa5a73acb49
Igor Barros <igorbarrosrj@gmail.com>
2020-08-28 18:10:06 -0300

    Create ProductController.js

---
+++ src/app/controllers/ProductController.js
@@ -0,0 +1,90 @@
+const { formatPrice } = require ('../../config/lib/utils')
+const Category = require('../models/Category')
+const Product = require('../models/Product')
+const Files = require('../models/Files')
+
+module.exports = {
+    create(req, res){
+    Category.all()
+    .then(function(results){
+
+       const categories = results.rows
+
+        return res.render("products/create.njk", { categories })
+
+    }).catch(function(err){
+        throw new Error(err)
+
+    })
+
+    
+    },
+
+    async post(req, res){
+
+        const keys = Object.keys(req.body)
+
+        for(key of keys){
+            if(req.body[key] == ""){
+                return res.send('Por Favor preencha todos os campos')
+            
+        }
+    }
+
+       if(req.files.length == 0)
+       return res.send("Por Favor, envie as imagens")
+
+
+       
+        let results = await Product.create(req.body)
+        const productId = results.rows[0].id
+
+        const filesPromise = req.files.map(file => Files.create({...file, product_id: productId}))
+        await Promise.all(filesPromise)
+
+
+                 return res.redirect(`/products/${productId}/edit`)
+    },
+
+    async edit(req,res){
+        
+        
+        
+        let results = await Product.find(req.params.id)
+        const product = results.rows[0]
+
+        if(!product) return res.send("Produto não encontrado!")
+        
+        product.old_price = formatPrice(product.old_price)
+        product.price = formatPrice(product.price)
+     
+        results = await Category.all()
+        const categories = results.rows
+         
+        return res.render("products/edit.njk", { product, categories })
+    },
+    async put (req, res){
+        const keys = Object.keys(req.body)
+
+        for(key of keys){
+            if(req.body[key] == ""){
+                return res.send('Por Favor preencha todos os campos')
+            }
+        }
+            req.body.price = req.body.price.replace(/\D/g, "")
+
+            if(req.body.old_price != req.body.price){
+                const oldProduct = await Product.find(req.body.id)
+
+                req.body.old_price = oldProduct.rows[0].price
+            }
+
+            await Product.update(req.body)
+            return res.redirect(`/products/${req.body.id}/edit`)
+        },
+    async delete(req, res) {
+        await Product.delete(req.body.id)
+
+        return res.redirect('/products/create')
+    }    
+}
